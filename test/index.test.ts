d7c978f8e9730822f0e3ddb6ad22fcbeedf4e989
HOVOH <71901758+HOVOH@users.noreply.github.com>
2022-04-21 15:21:55 -0400

    lint

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,28 +1,29 @@
-import {ContractFactory, providers, Network} from "@hovoh/evmcontractsregistry";
-import {IQiDAOProtocol, qiDAOProtocol} from "../src";
+import {
+  ContractFactory,
+  providers,
+  Network,
+} from '@hovoh/evmcontractsregistry';
+import { IQiDAOProtocol, qiDAOProtocol } from '../src';
 
 describe('ContractFactory', () => {
   let contractFactory: ContractFactory<IQiDAOProtocol>;
 
-  beforeEach(function() {
-
+  beforeEach(function () {
     contractFactory = new ContractFactory(providers, qiDAOProtocol);
   });
 
   it('Should return the network contract factory', () => {
     const networkContractFactory = contractFactory.forNetwork(
-        Network.OPERA_MAIN_NET
+      Network.OPERA_MAIN_NET
     );
     expect(networkContractFactory.networkProvider).not.toBeNull();
   });
 
   it('Should return the contracts properly typed and initialised', async () => {
     const wftmVault = contractFactory
-        .forNetwork(Network.OPERA_MAIN_NET)
-        .getLatestContractInstance('WFTMVault');
-    const WFTM_ADDRESS = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83"
-    expect(await wftmVault.collateral()).toEqual(WFTM_ADDRESS)
+      .forNetwork(Network.OPERA_MAIN_NET)
+      .getLatestContractInstance('WFTMVault');
+    const WFTM_ADDRESS = '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83';
+    expect(await wftmVault.collateral()).toEqual(WFTM_ADDRESS);
   });
-
 });
-
